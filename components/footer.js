863988ca9138b0b4bf5b580d750a26e78f5cf4b1
unknown <braiantucker@gmail.com>
2025-07-08 20:28:00 -0300

    quinto commit

--- components/footer.js
+++ components/footer.js
@@ -4,15 +4,15 @@ function footerComponent(el) {
     <footer class="footer_container">
   <img src="logo-desafio.png" class="footer_logo">
   <div class="footer_links">
-    <a href="index.html" class="footer_home"><i class="fas fa-home">Home</i></a>
-    <a href="servicios.html" class="footer_services"><i class="fas fa-user"></i>Servicios</a>
-    <a href="contacto.html" class="footer_contact"><i class="fas fa-phone"></i>
+      <a href="web-componentes/index.html" class="footer_home"><i class="fas fa-home">Home</i></a>
+      <a href="web-componentes/servicios.html" class="footer_services"><i class="fas fa-user"></i>Servicios</a>
+      <a href="web-componentes/contacto.html" class="footer_contact"><i class="fas fa-phone"></i>Contacto</a>
 Contacto</a>
   </div>
   <div class="footer_social-links">
-    <img src="logo-linkedin.jpg" class="footer_logo-linkedin">
-    <img src="logo-github.png" class="footer_logo-github">
-    <img src="logo-twitter.png" class="footer_logo-twitter">
+    <img src="web-componentes/logo-linkedin.jpg" class="footer_logo-linkedin">
+    <img src="web-componentes/logo-github.png" class="footer_logo-github">
+    <img src="web-componentes/logo-twitter.png" class="footer_logo-twitter">
   </div>
   <p class="footer_text">©2025 - https://apx.school</p>
  </footer>`;
